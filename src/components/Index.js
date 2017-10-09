fc18f8901ba18eefb4564a931ef8ef3fb739ff58
Christian John Saclao <vlade1356@gmail.com>
2017-10-09 20:43:17 +0800

    init Jennaj react

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,12 +4,6 @@ import { routerActions } from 'react-router-redux'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import Hotel from './Hotel/Index';
-import CountVowels from './CountVowels/Index';
-import FizzBuzz from './FizzBuzz/Index';
-import CinemaApp from './CinemaApp/Index';
-import ReverseString from './ReverseString';
-import Palindrome from './Palindrome/Index'
 
 require('react-progress-2/main.css');
 
@@ -32,21 +26,7 @@ require('react-progress-2/main.css');
 
     return (
       <div className="UserGrid">
-      <Tabs activeKey={this.state.key} onSelect={this.handleSelect} id="controlled-tab-example">
-
-        <Tab eventKey={1} title="Hotel Reservation System "><Hotel /></Tab>
-
-        <Tab eventKey={2} title="Fizz Buzz"> <FizzBuzz /> </Tab>
-
-        <Tab eventKey={3} title="Reverse a String" > <ReverseString /> </Tab>
-
-        <Tab eventKey={4} title="Count Vowels"> <CountVowels /></Tab>
-
-        <Tab eventKey={5} title="Check if Palindrome"> <Palindrome /></Tab>
-
-        <Tab eventKey={6} title="Cinema App"> <CinemaApp /> </Tab>
-
-      </Tabs>
+        Jennah
       </div>
     )
   }
